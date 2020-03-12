cb8ae3f81848105b3c8a814c41baae1cd6c7a029
caglorithm <sevensirk@gmail.com>
2020-03-12 23:24:05 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="mopet",
+    version="0.1",
+    description="Mildly ominous parameter exploration toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/caglorithm/mopet",
+    author="Caglar Cakan",
+    author_email="cakan@ni.tu-berlin.de",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    include_package_data=True,
+)
