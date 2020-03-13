7d95bd7c6d23b3003dc025cb220d912ad3541c2f
caglorithm <sevensirk@gmail.com>
2020-03-13 02:34:27 +0100

    testing

---
+++ tests/test_exploration.py
@@ -0,0 +1,29 @@
+import unittest
+import mopet
+import numpy as np
+
+
+class TestExploration(unittest.TestCase):
+    """Test full exploration.
+    """
+
+    def test_exploration(self):
+        def evalFunction(params):
+            result_float = abs((params["x"] ** 2 + params["y"] ** 2) - 1)
+            result_array = np.random.randn(
+                np.random.randint(1, 131), np.random.randint(1, 5000)
+            )
+            result = {}
+            result["float_result"] = result_float
+            result["array_result"] = result_array
+            return result
+
+        params = {"x": 1.3, "y": 2.7, "z": 0.0}
+        explore_params = {"x": np.linspace(-2, 2, 2), "y": np.linspace(-2, 2, 2)}
+
+        ex = mopet.Exploration(evalFunction, params, explore_params)
+
+        ex.run()
+        ex.load_results()
+        ex.df
+
